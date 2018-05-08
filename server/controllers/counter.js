1dc3d43cff1739a8e367d2fd9ad3ea767872a97d
李 宗儒 <34096454+Jasonlizongru@users.noreply.github.com>
2018-05-08 21:54:03 +0800

    '5.8.21.53'

--- server/controllers/counter.js
+++ server/controllers/counter.js
@@ -11,10 +11,9 @@ module.exports = async ctx => {
 
   rerr = await mysql("stuinfo").where({ stuid: selected}).select()
 
-  // rerr = await mysql('stuinfo').innerJoin('nameinfo', 'stuinfo.stuid', 'nameinfo.stuid').where({ stuid: selected }).select()
-
-  // rerr = mysql.select('*').from('stuinfo').rightJoin('nameinfo', 'stuinfo.stuid', 'nameinfo.stuid').where({ stuid: selected })
-
+  
+  // rerr = await mysql.table('stuinfo').innerJoin('nameinfo', 'stuinfo.stuid', '=', 'nameinfo.stuid').select()
+//连接操作，连接nameinfo和stuinfo
 
   ctx.state.data = rerr
 }
